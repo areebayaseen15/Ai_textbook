25c5cec684d5f7da464d64685d0ab8db7fea3ab6
Areeba Yaseen <147875287+areebayaseen15@users.noreply.github.com>
2025-12-15 02:49:36 +0500

    Update docusaurus.config.ts

--- mybook/docusaurus.config.ts
+++ mybook/docusaurus.config.ts
@@ -3,7 +3,7 @@ import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'Physical Ai Humanoid Textbook',
+  title: 'Physical Ai Humanoid Robotics Textbook',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.webp',
 
